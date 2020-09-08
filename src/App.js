99e3b9afeb70f14739b29fc357d1a54995ca43ab
Balazs Danyi <danyi.balage@gmail.com>
2020-09-08 17:04:51 +0100

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 
+import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div>
-      
+      <Header />
+      <About />
+      <Projects />
+      <Footer />
     </div>
   );
 }
