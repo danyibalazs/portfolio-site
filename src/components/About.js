4a5aafb8a78e7e94bb2da29b491c44d05f413e13
Balazs Danyi <danyi.balage@gmail.com>
2020-09-09 18:26:02 +0100

    basic about section

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,40 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      About
-    </div>
+    <section>
+      <div className="main-container">
+        <div className="about-wrapper">
+          <div className="more-column">
+            <h3>More about me</h3>
+            <p className="about-p">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed felis pulvinar, pharetra ipsum vitae, dapibus nunc. Praesent vel interdum risus. Quisque non magna vitae arcu laoreet mattis in tincidunt diam. Mauris ut ante justo. Mauris vulputate nulla consequat interdum varius.
+            </p>
+          </div>
+          <div className="expertise-column">
+            <h3>My Expertise</h3>
+            <p className="about-p">
+            Junior Web Developer with primary focus on JavaScript and React
+            </p>
+            <div id="terminal">
+              <ul>
+                <li>JavaScript</li>
+                <li>JavaScript</li>
+                <li>JavaScript</li>
+                <li>JavaScript</li>
+                <li>JavaScript</li>
+              </ul>
+              <ul>
+                <li>JavaScript</li>
+                <li>JavaScript</li>
+                <li>JavaScript</li>
+                <li>JavaScript</li>
+                <li>JavaScript</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -10,15 +10,14 @@ const Header = () => {
           <div className="greeting-wrapper">
             <h1>Hi, I am Balazs Danyi</h1>
           </div>
-
           <div className="intro-wrapper">
             <div className="left-column">
-              <img id="profile-pic" src={profilePic} />
+              <img id="profile-pic" src={profilePic} alt="Profile" />
             </div>
             <div className="right-column">
-              <h2 id="short-title">Who I am</h2>
+              <h2 >What I do</h2>
               <p>
-                I am a career entrant Web Developer, focusing on JavaScript related technologies.
+                I am a career entrant Web Developer
               </p>
             </div>
           </div>
