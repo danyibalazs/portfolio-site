515aa19156470dd309f284c89a7fe052ea657acf
Balazs Danyi <danyi.balage@gmail.com>
2020-09-09 13:06:18 +0100

    basic header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,17 @@ const Header = () => {
         <div className="background-image">
           <div className="greeting-wrapper">
             <h1>Hi, I am Balazs Danyi</h1>
-          </div>   
+          </div>
+
+          <div className="intro-wrapper">
+            <div className="right-column">
+              <h2>Who I am</h2>
+              <p>
+                I am a career entrant Web Developer,
+                focusing on JavaScript related technologies.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </header>
