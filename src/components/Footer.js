17bdc262c7b1cb230dd12be009d1ea7d2e1e5d41
Balazs Danyi <danyi.balage@gmail.com>
2020-09-14 11:26:46 +0100

    server for email sending added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import emailjs from 'emailjs-com';
+import axios from 'axios';
 
 const Footer = () => {
+
+  const [name, setName] = useState('');
+  const [subject, setSubject] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  // const [disabled, setDisabled] = useState(false);
+  const [emailSent, setEmailSent] = useState(null);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      name,
+      subject,
+      email,
+      message
+    }
+
+    axios.post('/api/email', data)
+      .then((res) => {
+        console.log(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+
+    e.target.reset();
+  }
+
   return (
     <footer>
       <div className="main-container">
         <div className="footer-wrapper">
           <div className="form-area">
-          <h3 id="form-title">Get in touch</h3>
-          <form id="contact-form">
-            <input className="input-field" type="text" placeholder="Name"/>
-            <input className="input-field" type="text" placeholder="Subject"/>
-            <input className="input-field" type="text" placeholder="Email"/>
-            <textarea className="input-field" placeholder="Message" />
-            <button id="submit-btn" type="submit">Send</button>
-          </form>
+            <h3 id="form-title">Get in touch</h3>
+            <form id="contact-form" onSubmit={onSubmit}>
+              <input 
+                className="input-field" 
+                type="text" 
+                placeholder="Name" 
+                required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input 
+                className="input-field" 
+                type="text" 
+                placeholder="Subject" 
+                required
+                value={subject}
+                onChange={(e) => setSubject(e.target.value)}
+              />
+              <input 
+                className="input-field" 
+                type="email" 
+                placeholder="Email" 
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <textarea 
+                className="input-field" 
+                placeholder="Message" 
+                required
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              <button id="submit-btn" type="submit">Send</button>
+            </form>
+          </div> 
+          <div className="message-area">
+             {/* <p>Email Sent</p> */}
           </div>
-          
         </div>
       </div>
 
